3558536381e69990385e36559af2ee037c7391c4
Jean Lazareva <ornaby@gmail.com>
2020-10-10 23:09:03 -0400

    Updates

---
+++ precache-manifest.e33e2a0fcc95c34251aa24e26249dc98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4eb742e8241c4bdcf43745978117c9a",
+    "url": "/ufox-react/index.html"
+  },
+  {
+    "revision": "3cf357b975db1ca7a116",
+    "url": "/ufox-react/static/css/main.4cc8e4ac.chunk.css"
+  },
+  {
+    "revision": "79dc31c48d2c82f8ec05",
+    "url": "/ufox-react/static/js/2.ce7b88b8.chunk.js"
+  },
+  {
+    "revision": "f7920831dd60393c4a4af0ab3777f1bd",
+    "url": "/ufox-react/static/js/2.ce7b88b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf357b975db1ca7a116",
+    "url": "/ufox-react/static/js/main.0923bd7f.chunk.js"
+  },
+  {
+    "revision": "fd1dd1bc27870bf352dc",
+    "url": "/ufox-react/static/js/runtime-main.6730c4dd.js"
+  },
+  {
+    "revision": "700e38407af499f51622c3a2a83b1ebc",
+    "url": "/ufox-react/static/media/backgroundSky.700e3840.jpg"
+  }
+]);
